1fd484ff2a4ea9c0f9f2863882c587607a58e813
Cristian Alderete <catastones@gmail.com>
2023-09-27 16:09:03 -0300

    error en proyecto se renueva carpeta

--- BackEnd_eCommerce/src/app.js
+++ BackEnd_eCommerce/src/app.js
@@ -3,20 +3,15 @@ import morgan from 'morgan';
 const cors = require('cors');
 import pkj from '../package.json';
 import './database'
-import {createRoles} from './libs/initrialSetup'
-const bodyParser = require('body-parser');
-
 
 
 const app = express();
 app.use(cors({
     origin:"*"
 }));
-createRoles();
+
 app.use(morgan('dev'));
 
-
-app.use(bodyParser.json({limit: "50mb", strict: true}));
 app.use(express.json());
 
 ;
--- BackEnd_eCommerce/src/database.js
+++ BackEnd_eCommerce/src/database.js
@@ -2,5 +2,5 @@ const mongosse = require('mongoose');
 require('dotenv').config();
 
 mongosse.connect(process.env.MONGO_URL)
-    .then(()=> console.log('Conectado a AtlasDB'))
+    .then(()=> console.log('Conectado a MongoDB Atlas'))
     .catch((error)=> console.log(error))
