d36ccf4cf22467bab485c5a5590a5b00116c5d55
Cristian Alderete <catastones@gmail.com>
2023-09-27 17:34:32 -0300

    pruebas crear user y login

--- BackEnd_eCommerce/src/controller/user.controller.js
+++ BackEnd_eCommerce/src/controller/user.controller.js
@@ -1,11 +1,12 @@
 import	User from '../models/User';
-import	Role from '../models/Roles';
+import	Roles from '../models/Roles';
 import jwt from 'jsonwebtoken';
+import config from '../config';
 
 export const singnup = async (req, res )=>{
     const { username, password, nombre, apellido, roles} = req.body;
     
-    const roleDB = await Role.find({name : roles});
+    const roleDB = await Roles.find({name : roles});
     console.log(roleDB);
      // creating a new User
      const user = new User({
@@ -21,8 +22,22 @@ export const singnup = async (req, res )=>{
     console.log(user);
     const saveUser =  await user.save();
                     
-   const token =  jwt.sign({id:saveUser._id},config.SECERET,{expiresIn:86400});
+   const token =  jwt.sign({id:saveUser._id},config.SECRET,{expiresIn:86400});
    saveUser.password = null;   
    saveUser.token = token;
    res.json(saveUser);
+}
+export const singnin = async (req, res )=>{
+    const userFound = await User.findOne({username : req.body.username})
+                                                             
+   
+    if (!userFound) return res.status(400).json({message: 'username incorrecto'});
+    const passMach = await User.comparePassword(req.body.password ,userFound.password )
+    if (!passMach) return res.status(400).json({message: 'password incorrecto'});
+    userFound.password = null;
+    const token =  jwt.sign({id:userFound._id},config.SECRET,{expiresIn:86400});
+    userFound.token = token;
+    res.json(userFound);     
+  
+    
 }
--- BackEnd_eCommerce/src/routes/user.routes.js
+++ BackEnd_eCommerce/src/routes/user.routes.js
@@ -5,10 +5,11 @@ import * as userCtrl from '../controller/user.controller'
 
 
  router.post('/newuser',userCtrl.singnup);
+ router.post('/login',userCtrl.singnin);
 // router.get('/', userCtrl.listarUser);
 // router.get('/:userId',userCtrl.buscarUserById);
 // router.post('/newuser',[verify.checkRolesExisted,verify.checkDuplicateUsername],userCtrl.singnup);
-// router.post('/login',userCtrl.singnin);
+
 // router.delete('/:userId',userCtrl.borarrUserById);
 // router.put('/:userId',userCtrl.updateUser);
 
