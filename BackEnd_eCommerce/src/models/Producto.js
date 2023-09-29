556b2a4a2c0c61277b278bfc48c4778cdf473854
Cristian Alderete <catastones@gmail.com>
2023-09-29 11:10:00 -0300

    se genera modelo Producto

---
+++ BackEnd_eCommerce/src/models/Producto.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+const clienteSchema = new Schema({
+    categoria:{
+        type: String,
+        require: true,       
+        
+    },
+    modelo:{
+        type: String,
+        require: true
+        
+    },
+    marca:{
+        type: String,
+        require: true,
+        index: { unique: true }         
+    },
+    descripcion:{
+        type: String,
+        require: true
+        
+    },
+    stock:{
+        type: Number,
+        require: true
+    },    
+    
+    precio:{
+        type: Number,
+        require: true
+    }  
+
+},{timestamps:true ,
+    versionKey:false});
