9293747b950ec40e511db4144e2f4e61fd43f7ca
Cristian Alderete <catastones@gmail.com>
2023-10-02 16:33:57 -0300

    se generan funciones CRUD en categorias controller

--- BackEnd_eCommerce/src/controller/categoria.controller.js
+++ BackEnd_eCommerce/src/controller/categoria.controller.js
@@ -1,6 +1,30 @@
 import	Categoria from '../models/Categoria';
 
-export const newCategoria = async (req, res )=>{}
-export const getCategorias = async (req, res )=>{}
-export const deleteCategorias = async (req, res )=>{}
-export const updateCategorias = async (req, res )=>{}
+export const newCategoria = async (req, res )=>{
+    const {name} = req.body;
+    const nameExist = await Categoria.find({name:name})
+    if(nameExist) return res.json({message: 'La Categoria ya existe'})
+        const newCat = categoriaSchema(name);
+    await newCat.save()
+                .then((data)=> res.json(data))
+                .catch((error)=> res.json({message : error})) 
+}
+
+export const getCategorias = async (req, res )=>{
+   await Categoria.find()
+                .then((data)=> res.json(data))
+                .catch((error)=> res.json({message : error})) 
+}
+export const deleteCategorias = async (req, res )=>{
+    const {name} = req.body;
+    await Categoria.deleteOne({ name: name })
+                .then(()=> res.status(201).json({message : `Categoria ${name} eliminada`}))
+                .catch((error)=> res.json({message : error})) ; 
+
+}
+export const updateCategorias = async (req, res )=>{
+        const {id, name} = req.body;        
+    await Categoria.findByIdAndUpdate(id, { name: name })
+                .then(()=> res.status(201).json({message : `Categoria ${name} actualizada`}))
+                .catch((error)=> res.json({message : error})) ; 
+}
