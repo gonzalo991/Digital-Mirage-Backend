edb88d38b2a4bd34a36d46a767afeff2226b0a82
Cristian Alderete <catastones@gmail.com>
2023-10-02 16:16:19 -0300

    categoria controller

---
+++ BackEnd_eCommerce/src/controller/categoria.controller.js
@@ -0,0 +1,6 @@
+import	Categoria from '../models/Categoria';
+
+export const newCategoria = async (req, res )=>{}
+export const getCategorias = async (req, res )=>{}
+export const deleteCategorias = async (req, res )=>{}
+export const updateCategorias = async (req, res )=>{}
