0dde87e67ce3b3966f35b0c21aa8a35cfa052942
Gonzalo Araya <gonxaraya991@gmail.com>
2024-07-29 17:04:07 -0300

    arreglos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import app from './app';
+import 'dotenv/config';
 import config from './config';
 
-// Cambiar el puerto de 8080 a 3055
 const port = config.PORT;
 const host = config.HOST;
 
-app.listen(port, host, () => {
-  console.log(`App listening on port: ${host}:${port}`);
+app.listen(port, host , () => {
+  console.log(`App listening on port: ${host} : ${port}`);
 });
