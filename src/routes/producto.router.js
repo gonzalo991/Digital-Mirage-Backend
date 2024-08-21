2ad5eee16d23a8d90ab20d51409dc055238bc187
Gonzalo Araya <gonxaraya991@gmail.com>
2024-08-21 19:57:53 -0300

    cambio en rutas

--- src/routes/producto.router.js
+++ src/routes/producto.router.js
@@ -7,7 +7,7 @@ import * as productoCtrl from '../controller/producto.controller'
 router.post('/', productoCtrl.newProducto);
 router.get('/', productoCtrl.getProductos);
 router.get('/byid/:productoId', productoCtrl.getProductoById);
-router.get('/categoria/:categoria', productoCtrl.getProductoByCategoria);
+router.get('/:categoria', productoCtrl.getProductoByCategoria);
 router.get('/buscar/:keyword', productoCtrl.getProductByKeyword);
 router.delete('/:productoId', productoCtrl.deleteProducto);
 router.put('/', productoCtrl.updateProducto);
